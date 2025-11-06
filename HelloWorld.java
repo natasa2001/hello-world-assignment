abf38eab09874d29891a427a57f0032bea0508c6
natasa2001 <aleksanata22@icloud.com>
2025-11-06 18:12:16 +0100

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Zdravo iz Jave, Nataša!");
+        for (int i = 1; i <= 36; i++) {
+            System.out.println("Hello World");
+        }
     }
 }
